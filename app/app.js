50f6fd109e3b6c870d680899633f99f48fb4bebf
JinSu Kim <wlstn8559@naver.com>
2022-12-02 15:05:04 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+
+module.exports = app;
+
